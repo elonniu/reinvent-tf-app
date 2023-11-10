b9a25322edd032b713d062cf7a40318ccfb2e326
Jingyi <elonniu@amazon.com>
2023-11-10 18:01:17 +0800

    fixed bug for shell

--- demo1/api.tf
+++ demo1/api.tf
@@ -1,35 +1,54 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name        = var.api_name
-  description = var.api_description
+  name = "Terraform REST Example"
 }
 
-resource "aws_api_gateway_resource" "api_resource" {
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  triggers    = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.open_resource.id,
+      aws_api_gateway_method.open_get_method.id,
+      aws_api_gateway_integration.open_integration.id,
+    ]))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_log_group" "logs" {
+  name = "/aws/vendedlogs/tf_rest_logs"
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.logs.arn
+    format = jsonencode({"requestId":"$context.requestId", "ip":"$context.identity.sourceIp", "requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","routeKey":"$context.routeKey", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength", "integrationError":"$context.integrationErrorMessage" })
+  }
+}
+
+resource "aws_api_gateway_resource" "open_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = var.api_path_part
+  path_part   = "open"
 }
 
-resource "aws_api_gateway_method" "ApiMethod" {
+resource "aws_api_gateway_method" "open_get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.open_resource.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_integration" {
+resource "aws_api_gateway_integration" "open_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.ApiMethod.http_method
+  resource_id             = aws_api_gateway_resource.open_resource.id
+  http_method             = aws_api_gateway_method.open_get_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
+  content_handling        = "CONVERT_TO_TEXT"
   uri                     = aws_lambda_function.lambda.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.api_integration,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = var.api_stage_name
 }
--- demo1/outputs.tf
+++ demo1/outputs.tf
@@ -3,13 +3,9 @@ output "lambda_function_name" {
 }
 
 output "api_gateway_invoke_url" {
-  value = aws_api_gateway_deployment.api_deployment.invoke_url
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
 
 output "lambda_function_console_url" {
   value = "https://console.aws.amazon.com/lambda/home?region=${var.aws_region}#/functions/${aws_lambda_function.lambda.function_name}"
-}
-
-output "api_gateway_console_url" {
-  value = "https://console.aws.amazon.com/apigateway/home?region=${var.aws_region}#/apis/${aws_api_gateway_rest_api.api.id}/resources/${aws_api_gateway_resource.api_resource.id}/methods/POST"
 }
